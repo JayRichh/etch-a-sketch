91f265c546cc17ba35ef8794bf509025f52f2d1c
Jayden Richardson <jsrich@hotmail.co.nz>
2022-07-11 14:59:27 +1200

    to finish change color on mouse click

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 "use strict";
 
 const content = document.getElementById("content");
-
 const btn = document.createElement("button");
-btn.innerText = "Create Table";
+
+btn.innerText = "Create New Table";
 btn.onclick = function () {
     alert("How many squares would you like in the table?");
     const input = prompt("Enter a number");
     const num = parseInt(input);
-    if (num > 0) {
+    if (num > 0 && num < 100) {
         createTable(num);
     } else if (num === 0) {
         alert("Mate, must be larger than 0.");
     } else if (num > 100) {
         alert("Please keep it under 100!");
+    } else if (num === null) {
+        alert("You must enter a number!");
     }
 };
 content.appendChild(btn);
 
-const tbody = document.createElement("tbody");
 const tbl = document.createElement("table");
+const tbody = document.createElement("tbody");
 tbl.style.width = "960px";
 tbl.style.height = "960px";
+var rowCount = tbl.rows.length;
 function createTable(num) {
+    while (tbody.hasChildNodes()) {
+        tbody.removeChild(tbody.firstChild);
+    }
     for (let i = 0; i < num; i++) {
         const tr = document.createElement("tr");
         for (let j = 0; j < num; j++) {
             const td = document.createElement("td");
-            const cell = document.createElement("div");
+            var cell = document.createElement("div");
             cell.className = "cell";
             cell.style.backgroundColor = "white";
             cell.style.width = "100%";
             cell.style.height = "100%";
             cell.style.border = "1px solid black";
+
             td.appendChild(cell);
             tr.appendChild(td);
         }
